23da9ee824f366ef964516f0f8e8f342ffce9316
DevDoshcoin <152967252+DevDoshcoin@users.noreply.github.com>
2023-12-05 14:33:32 +0500

    doshcoin.sol

--- doshcoin.sol
+++ doshcoin.sol
@@ -1,1 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DoshCoin is ERC20 {
+    constructor(uint256 totalSupply) ERC20("Doshcoin", "DOSH") {
+        _mint(msg.sender, totalSupply);
+    }
+}
